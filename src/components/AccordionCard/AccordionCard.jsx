5a0d2ce875e6de93056c5e192b99b0a2333df3b4
FunnyMan11 <kobuladze.nk1@gmail.com>
2023-01-31 01:24:06 +0400

    with mapping

--- src/components/AccordionCard/AccordionCard.jsx
+++ src/components/AccordionCard/AccordionCard.jsx
@@ -17,55 +17,36 @@ export default function AccordionCard() {
     {
       itemNumber: 1,
       title: "Accordion 1 title",
-      content: "ravi ragaca 1 "
+      content: "ravi ragaca 1 ",
     },
     {
       itemNumber: 2,
       title: "Accordion 2 title",
-      content: "ravi ragaca 2 "
+      content: "ravi ragaca 2 ",
     },
     {
       itemNumber: 3,
       title: "Accordion 3 title",
-      content: "ravi ragaca 3 "
+      content: "ravi ragaca 3 ",
     },
     {
       itemNumber: 4,
       title: "Accordion 4 title",
-      content: "ravi ragaca 4 "
+      content: "ravi ragaca 4 ",
     },
-  ]
+  ];
 
   return (
     <div className={styles.cardContainer}>
-        <AccordionItem 
-          activeAccordionNumber ={activeAccordionNumber}
+      {items.map((item) => (
+        <AccordionItem
+          activeAccordionNumber={activeAccordionNumber}
           handleClick={handleClick}
-          itemNumber={1}
-          title = "Accordion title"
-          content = "22222"
+          itemNumber={item.itemNumber}
+          title={item.title}
+          content={item.content}
         />
-        <AccordionItem 
-          activeAccordionNumber ={activeAccordionNumber}
-          handleClick={handleClick}
-          itemNumber={2}
-          title = "Accordion title"
-          content = "22222"
-        />
-        <AccordionItem 
-          activeAccordionNumber ={activeAccordionNumber}
-          handleClick={handleClick}
-          itemNumber={3}
-          title = "Accordion title"
-          content = "22222"
-        />
-        <AccordionItem 
-          activeAccordionNumber ={activeAccordionNumber}
-          handleClick={handleClick}
-          itemNumber={4}
-          title = "Accordion title"
-          content = "22222"
-        />
+      ))}
     </div>
   );
 }
